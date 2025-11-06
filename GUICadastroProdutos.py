857bcef4b6964f158fd478a71df56835fc7a0eed
leosturmer <leo@email.com>
2025-11-06 10:30:43 -0300

    tela cadsatro de produtos com labels e entrys

--- GUICadastroProdutos.py
+++ GUICadastroProdutos.py
@@ -9,5 +9,16 @@ class CadastroProdutos:
         self.root.title("Cadastro de Produtos")
         self.root.geometry("270x150")
 
+        # Labels e entradas para os dados dos produtos
 
+        tk.Label(self.root, text="Nome: ").grid(row=0, column=0)
+        self.entry_nome = tk.Entry(self.root)
+        self.entry_nome.grid(row=0, column=1)
 
+        tk.Label(self.root, text="Valor: ").grid(row=1, column=0)
+        self.entry_valor = tk.Entry(self.root)
+        self.entry_valor.grid(row=1, column=1)
+        
+        tk.Label(self.root, text="Quantidade: ").grid(row=2, column=0)
+        self.entry_quantidade = tk.Entry(self.root)
+        self.entry_quantidade.grid(row=2, column=1)
